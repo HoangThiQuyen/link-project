e66f8fd682c46664127147ee5e87d4fdf3694c86
quyen <hoangquyen99@gmail.com>
2022-05-08 16:31:46 +0700

    add toast and add current link

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+function launch_toast() {
+  var x = document.getElementById("toast__current")
+  x.classList.add('toast')
+  setTimeout(function(){
+     x.className = x.className.replace("toast", "");
+     
+     }, 2000);
+}
+
+function launch_toast__error() {
+  var x = document.getElementById("toast__current__error")
+  x.classList.add('toast_error')
+  setTimeout(function(){ x.className = x.className.replace("toast_error", ""); }, 2000);
+}
+
+function add_class_hidden() {
+ document.getElementById("link__current").style.visibility='inherit'
+}
 
 document.getElementById('btnSubmit').addEventListener("click",(event)=>{
   let valueLink = document.getElementById('link').value.trim();
@@ -6,12 +24,15 @@ document.getElementById('btnSubmit').addEventListener("click",(event)=>{
     axios.post('https://link-server.vercel.app/api/v1/project', {link:valueLink})
     .then(function (response) {
       document.getElementById('link').value=''
+      launch_toast()
+      document.getElementById('value_link').innerHTML = valueLink;
+      add_class_hidden()
     })
     .catch(function (error) {
       console.log(error);
     });
   }else{
-    alert('Please enter link')
+    launch_toast__error()
   }
 })
 
