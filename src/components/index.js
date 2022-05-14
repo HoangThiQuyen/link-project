86339d6e430592772447caba6d0a95a9a4157daf
quyen <hoangquyen99@gmail.com>
2022-05-14 10:24:09 +0700

    fix many toast and add logo

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import { Button, Input } from "reactstrap";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { API } from "../utils/axios";
+import imgLogo from "../img/logo.png";
+
 const InputFile = () => {
   const [value, setValue] = useState("");
   const [curentLink, setCurentLink] = useState("");
@@ -10,38 +12,43 @@ const InputFile = () => {
   const handleSubmit = () => {
     if (!value.trim()) {
       toast.error("Vui lòng nhập link!");
+      toast.clearWaitingQueue();
     } else {
       // API.postAPI({ link: value }).then(() => {
-      //   API.getAPI().then((res) => setCurentLink(res ? res.link : ""));
+      //   API.getAPI().then((res) => {
+      //     setCurentLink(res ? res.link : "");
+      //     setValue("");
+      //   });
       // });
-      setValue('')
-      toast.success("Cập nhật thành công!");
+      setValue("");
+      toast.success('Cập nhật thành công!')
     }
   };
   useEffect(() => {
     API.getAPI().then((res) => setCurentLink(res ? res.link : ""));
   }, []);
   return (
-    <div className="inputFile">
-      <div className="inputFile-container">
-        <p>
-          Link hiện tại:{" "}
-          <u>
-            <i>{curentLink}</i>
-          </u>
-        </p>
-        <Input
-          type="text"
-          placeholder="Nhập link tại đây ..."
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <Button color="warning" onClick={handleSubmit}>
-          Save
-        </Button>
-      </div>
+    <div>
+      <img src={imgLogo} alt="img-logo" className="img-fluid" />
+      <div className="inputFile">
+        <div className="inputFile-container">
+          <p>
+            Link hiện tại:{" "}
+            <u>
+              <i>{curentLink}</i>
+            </u>
+          </p>
+          <Input
+            value={value}
+            type="text"
+            placeholder="Nhập link tại đây ..."
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button onClick={handleSubmit}>Save</Button>
+        </div>
 
-      <ToastContainer limit={1} autoClose={1800} />
+        <ToastContainer limit={1} autoClose={1800} />
+      </div>
     </div>
   );
 };
